f92acdf9406bb76d3feb874e4cb4de9254fb1b2d
_nguyendu <1851061329@e.tlu.edu.vn>
2022-07-06 11:57:46 +0700

    abc

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,12 +1,50 @@
+const userModel = require('../models/user');
+
 class authController {
   login(req, res) {
-    res.render('login/login');
-  }
+    const username = req.body.username;
+    const password = req.body.password;
 
+    userModel.findOne({ username: username, password: password})
+      .then(data => {
+        if (data) {
+          res.redirect('/')
+        }
+        else {
+          res.redirect('back')
+        }
+      })
+      .catch(err => {
+        res.status(500).json("Lỗi")
+      })
+  }
+ 
   register(req, res) {
-    res.render('login/register');
-  }
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
 
-}
+    userModel.findOne({ email: email })
+      .then(data => {
+        if (data) {
+          res.json("Email đã tồn tại")
+        }
+        else {
+          return userModel.create({
+            email: email,
+            username: username,
+            password: password 
+          })
+        }
+      })
+      .then(data => {
+        res.redirect('/auth/login')
+      })
+      .catch(err => {
+        res.status(500).json("Tạo tài khoản thất bại")
+      })
+ 
+  }
+} 
 module.exports = new authController;
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,7 +6,13 @@ const authController = require('../app/controllers/authController');
 
 
 // GET route for reading data
-router.get('/login', authController.login);
-router.get('/register', authController.register);
+router.post('/login', authController.login);
+router.get('/login',(req, res) => {
+    res.render('login/login')
+});
+router.post('/register', authController.register);
+router.get('/register',(req, res) => {
+    res.render('login/register')
+})
 
 module.exports = router;
